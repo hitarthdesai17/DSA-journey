52e2f310ec7cd605ba1b68cae735d5750fbb972f
hitarthdesai17 <hitarthdesai184@gmail.com>
2026-06-14 21:08:59 +0530

    Added Some patterns , alternatives, and corrections

--- NESTED LOOPING/right_triangle.py
+++ NESTED LOOPING/right_triangle.py
@@ -11,3 +11,16 @@ for i in range(1,n+1):
     for j in range(1,i+1):
         print("*",end=" ")#For number print j
     print("")
+
+"""
+n = int(input())  # stdin: 5
+
+for i in range(1, n + 1):
+    print("* " * i)
+
+"""
+#For Alphabets
+for i in range(1, 6):
+    for j in range(i):
+        print(chr(65 + j), end=" ")
+    print()
